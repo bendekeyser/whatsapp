925c49856d5caf8a95f7dcb49c60d909bb0705e6
VicDK <r0946943@student.thomasmore.be>
2025-02-20 16:37:42 +0100

    push 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['puppeteer-core', 'puppeteer'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        'puppeteer-core': 'puppeteer-core',
+        puppeteer: 'puppeteer',
+      });
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
